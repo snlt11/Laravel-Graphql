a3b4fad2f83b917240d20be2ae61a86069e0ff1e
sainaylinthar <sainaylinthar@gmail.com>
2024-10-09 09:39:19 +0630

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'super@admin.com',
+            'position' => 'Super Administrator',
+            'salary' => '100000',
+            'role' => 'admin',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+        ]);
+        User::factory(10)->create();
+    }
+}
