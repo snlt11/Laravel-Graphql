db396d1fba4f80a05b16edee3e90ac86259b3eef
sainaylinthar <sainaylinthar@gmail.com>
2024-10-09 09:47:54 +0630

    feat: setup laravel passport

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'position' => fake()->jobTitle(),
+            // 'salary' => Hash::make(fake()->randomNumber(5)),
             'salary' => fake()->randomNumber(5),
             'role' => 'user',
             'email_verified_at' => now(),
@@ -40,7 +41,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
